7ad9956900161663d096d8742f15947012d8ae57
Simon Grossi <simon.grossi@gmail.com>
2025-07-29 13:13:03 +0200

    fix(utils): Renforcer le calcul du coût de la voiture
    Améliore la robustesse de la fonction `calculer_couts_voiture` en remplaçant l'accès direct aux clés du dictionnaire par la méthode `.get()`.
    
    Cette modification empêche l'application de crasher avec une `KeyError` si un ou plusieurs champs du formulaire de la voiture ne sont pas renseignés par l'utilisateur. En cas de clé manquante, une valeur par défaut est désormais utilisée, garantissant la stabilité du calcul et une meilleure expérience utilisateur.

--- utils.py
+++ utils.py
@@ -48,17 +48,28 @@ def calculer_couts_voiture(params: dict) -> CoutVoitureResultats:
     Calcule le coût TCO annuel d'une voiture et retourne un objet CoutVoitureResultats.
     """
     try:
-        amortissement = (params['prix_achat'] - params['valeur_revente']) / params['duree_possession']
-        cout_carburant = (params['km_annuels'] / 100) * params['consommation'] * params['prix_carburant']
-        cout_fixe_annuel = params['assurance'] + params['entretien'] + params['autres_frais']
+        # --- DEBUT DE LA MODIFICATION ---
+        # Utilisation de .get() pour éviter les erreurs si une clé est absente
+        # et pour fournir une valeur par défaut.
+        amortissement = (params.get('prix_achat', 0) - params.get('valeur_revente', 0)) / params.get('duree_possession', 1)
+        cout_carburant = (params.get('km_annuels', 0) / 100) * params.get('consommation', 0) * params.get('prix_carburant', 0)
+        
+        assurance = params.get('assurance', 0)
+        entretien = params.get('entretien', 0)
+        autres_frais = params.get('autres_frais', 0)
+
+        cout_fixe_annuel = assurance + entretien + autres_frais
         cout_total_annuel = amortissement + cout_carburant + cout_fixe_annuel
         
         details = {
             "Amortissement": amortissement, "Carburant": cout_carburant,
-            "Assurance": params['assurance'], "Entretien": params['entretien'],
-            "Autres frais": params['autres_frais']
+            "Assurance": assurance, "Entretien": entretien,
+            "Autres frais": autres_frais
         }
         return CoutVoitureResultats(cout_annuel=cout_total_annuel, details=details)
+        # --- FIN DE LA MODIFICATION ---
         
-    except (ZeroDivisionError, KeyError):
+    except ZeroDivisionError:
+        # Cette sécurité est conservée au cas où duree_possession serait 0 via le .get(key, 0)
+        # même si nous avons mis une valeur par défaut de 1.
         return CoutVoitureResultats()
